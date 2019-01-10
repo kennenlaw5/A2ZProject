ff41d52c5c2e6d646466f437082ad91c4683a0e8
Kennen <kennen.lawrence@a2zsync.com>
2019-01-10 07:45:24 -0700

    Contain movies in box and move "Add" section to beneath box

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -6,12 +6,16 @@
 
 @section('content')
     <h1 class="title">Movies</h1>
+    @if ($movies->count())
+        <div class="box">
+        @foreach($movies as $movie)
+            <li>
+                <a href="/movies/{{ $movie->id }}">{{ $movie->title }}, released in {{ $movie->year }} ({{ $movie->age }} {{ $movie->age > 1 ? 'years' : 'year' }} ago)</a>
+            </li>
+        @endforeach
+        </div>
+    @endif
     <div class="container">
         <a href="/movies/create">Add New Movie</a>
     </div>
-    @foreach($movies as $movie)
-        <li>
-            <a href="/movies/{{ $movie->id }}">{{ $movie->title }}, released in {{ $movie->year }} ({{ $movie->age }} {{ $movie->age > 1 ? 'years' : 'year' }} ago)</a>
-        </li>
-    @endforeach
 @endsection
